945a04c7d26199cb2c9f1ffd52eb032209d64f65
Jeffy Chen <jeffy.chen@rock-chips.com>
2025-03-12 12:48:53 +0800

    gstreamer1: Bump to buildroot upstream's 1.22.12
    Bump to:
    c04ba4b580 Update for 2024.11.1
    
    With local patches.
    
    Change-Id: I1f734da484c53a3db4c74453487344344418ed33
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/gstreamer1/gst1-plugins-good/gst1-plugins-good.mk
+++ package/gstreamer1/gst1-plugins-good/gst1-plugins-good.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_PLUGINS_GOOD_VERSION = 1.22.9
+GST1_PLUGINS_GOOD_VERSION = 1.22.12
 GST1_PLUGINS_GOOD_SOURCE = gst-plugins-good-$(GST1_PLUGINS_GOOD_VERSION).tar.xz
 GST1_PLUGINS_GOOD_SITE = https://gstreamer.freedesktop.org/src/gst-plugins-good
 GST1_PLUGINS_GOOD_LICENSE_FILES = COPYING
@@ -361,7 +361,7 @@ endif
 
 ifeq ($(BR2_PACKAGE_GST1_PLUGINS_GOOD_PLUGIN_QMLGL),y)
 GST1_PLUGINS_GOOD_CONF_OPTS += -Dqt5=enabled
-GST1_PLUGINS_GOOD_DEPENDENCIES += qt5declarative
+GST1_PLUGINS_GOOD_DEPENDENCIES += qt5declarative qt5tools
 ifeq ($(BR2_PACKAGE_QT5BASE_XCB),y)
 GST1_PLUGINS_GOOD_DEPENDENCIES += qt5x11extras
 endif
