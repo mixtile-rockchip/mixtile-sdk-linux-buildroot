14c46b26c7849cc7397c160c581b5eb4701f8231
ZhengRong Ruan <ryan.ruan@rock-chips.com>
2025-04-21 11:32:39 +0800

    package: rockchip: ipc_drv_ko: support arm32 config
    Change-Id: I786c8325a7992c82a58f61f0a1364c920a5f0974
    Signed-off-by: ZhengRong Ruan <ryan.ruan@rock-chips.com>

--- package/rockchip/ipc_drv_ko/ipc_drv_ko.mk
+++ package/rockchip/ipc_drv_ko/ipc_drv_ko.mk
@@ -12,4 +12,12 @@ IPC_DRV_KO_INSTALL_STAGING = YES
 
 IPC_DRV_KO_CONF_OPTS += -DFOR_BUILDROOT=TRUE
 
+ifeq ($(call qstrip,$(BR2_ARCH)), arm)
+IPC_DRV_KO_ARCH = arm
+else ifeq ($(call qstrip, $(BR2_ARCH)), aarch64)
+IPC_DRV_KO_ARCH = arm64
+endif
+
+IPC_DRV_KO_CONF_OPTS += -DRK_APP_ARCH_TYPE=$(IPC_DRV_KO_ARCH)
+
 $(eval $(cmake-package))
