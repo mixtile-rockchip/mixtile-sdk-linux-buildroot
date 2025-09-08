b2dec55c50f36af941c416a5495cc66e585e39b3
Caesar Wang <wxt@rock-chips.com>
2025-09-08 09:38:39 +0800

    package/rockchip: add rockchip-rkai
    Change-Id: If5c4bfc760ed91c8291cb5a3d6d0faf68dd0b0e6
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

---
+++ package/rockchip/rockchip-ai/rockchip-ai.mk
@@ -0,0 +1,52 @@
+################################################################################
+#
+# rockchip-rkai
+#
+################################################################################
+
+ROCKCHIP_AI_VERSION = 1.0
+ROCKCHIP_AI_SITE = $(TOPDIR)/../app/rkai
+ROCKCHIP_AI_SITE_METHOD = local
+ROCKCHIP_AI_INSTALL_STAGING = YES
+ROCKCHIP_AI_INSTALL_TARGET = YES
+ROCKCHIP_AI_DEPENDENCIES = host-pkgconf host-cmake rknn-llm rknpu2
+ROCKCHIP_AI_LICENSE = ROCKCHIP
+ROCKCHIP_AI_LICENSE_FILES = LICENSE
+
+# Debug/Release/RelWithDebInfo
+ROCKCHIP_AI_BUILD_TYPE ?= Release
+
+# CMake
+define ROCKCHIP_AI_CONFIGURE_CMDS
+    mkdir -p $(@D)/build
+    cd $(@D)/build && \
+    $(TARGET_MAKE_ENV) $(HOST_DIR)/bin/cmake .. \
+        -DCMAKE_SYSTEM_PROCESSOR=$(BR2_ARCH) \
+        -DCMAKE_SYSTEM_NAME=Linux \
+        -DCMAKE_C_COMPILER="$(TARGET_CC)" \
+        -DCMAKE_CXX_COMPILER="$(TARGET_CXX)" \
+        -DCMAKE_BUILD_TYPE=$(ROCKCHIP_AI_BUILD_TYPE) \
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+        -DCMAKE_INSTALL_PREFIX=/usr
+endef
+
+define ROCKCHIP_AI_BUILD_CMDS
+    cd $(@D)/build && \
+    $(TARGET_MAKE_ENV) $(MAKE) -j$(PARALLEL_JOBS)
+endef
+
+define ROCKCHIP_AI_INSTALL_TARGET_CMDS
+    cd $(@D)/build && \
+    $(TARGET_MAKE_ENV) $(MAKE) install DESTDIR=$(TARGET_DIR)
+endef
+
+define ROCKCHIP_AI_INSTALL_STAGING_CMDS
+    cd $(@D)/build && \
+    $(TARGET_MAKE_ENV) $(MAKE) install DESTDIR=$(STAGING_DIR)
+endef
+
+ifeq ($(BR2_PACKAGE_RV1126B), y)
+    ROCKCHIP_AI_CONFIGURE_CMDS += -DROCKCHIP_AI_CHIP=rv1126b
+endif
+
+$(eval $(generic-package))
